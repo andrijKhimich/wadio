e603eb7401d58793e0c04587aff68ffde047cc3e
Andriy Khimich <andrykhimich@gmail.com.noreply.github.com>
2021-06-20 16:41:22 +0300

    start fix responcive

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,34 @@
-// let container, camera, renderer, scene, phone;
+const body = $('body');
+// call this to Disable
+function disableScroll() {
+  // window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  // window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  // window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  // window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+  // console.log('desabled');
+  body.addClass('no-scroll');
+}
 
+// call this to Enable
+function enableScroll() {
+  // window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  // window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  // window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  // window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+  // console.log('enabled');
+  // stopFlag = true;
+  setTimeout(function () {
+    body.removeClass('no-scroll');
+  }, 1000)
+}
+
+
+
+$(window).on('load', function () {
+  // alert('page is loaded');
+  console.log('page is loaded');
+
+});
 // const lottie = require("../libs/lottie.js");
 
 
@@ -173,26 +202,28 @@ const showOnLoadEl = $('.js-show-on-load');
 
 function preloader() {
 
-  // logo.addClass('animate');
+  logo.addClass('animate');
   setTimeout(function () {
     if ($('.header').hasClass('scrolled')) {
       logo.addClass('logo_animated');
     } else {
-      logo.removeClass('logo_animated');
-
-      logo.addClass('animate');
+      // logo.removeClass('logo_animated');
+      // logo.addClass('animate');
     }
   }, 100);
 
-  // showLogo();
+  showLogoAnimation();
+
   setTimeout(function () {
     heroBg.removeClass('js-active');
   }, 2000);
 
   setTimeout(function () {
     showOnLoadEl.removeClass('js-slide-top js-slide-down');
-  }, 3000);
+  }, 4000);
 }
+
+
 
 // main menu functionality
 const burger = $('.js-burger');
@@ -227,11 +258,29 @@ function loadHeroAnimation() {
       anim.pause();
     }
   });
+
+  window.addEventListener('resize', function () {
+    anim.resize();
+  });
 }
 
 setTimeout(function () {
   loadHeroAnimation();
 }, 3400);
+
+function showLogoAnimation() {
+  const logo = document.getElementById("logo"),
+    anim = lottie.loadAnimation({
+      container: logo,
+      renderer: "canvas",
+      loop: false,
+      autoplay: true,
+      path: "./json/loader.json"
+    });
+  window.addEventListener('resize', function () {
+    anim.resize();
+  });
+}
 
 function showDownloadIphone() {
   const downloadIphone = document.getElementById("downloadIphone"),
@@ -242,6 +291,9 @@ function showDownloadIphone() {
       autoplay: true,
       path: "./json/hero-iphone.json"
     });
+  window.addEventListener('resize', function () {
+    anim.resize();
+  });
 }
 
 function showDownloadPhone() {
@@ -253,6 +305,9 @@ function showDownloadPhone() {
       autoplay: true,
       path: "./json/download-phone.json"
     });
+  window.addEventListener('resize', function () {
+    anim.resize();
+  });
 }
 
 function showMusicPhone() {
@@ -264,6 +319,9 @@ function showMusicPhone() {
       autoplay: true,
       path: "./json/music-phone.json"
     });
+  window.addEventListener('resize', function () {
+    anim.resize();
+  });
 }
 
 function showSocialPhone() {
@@ -275,24 +333,30 @@ function showSocialPhone() {
       autoplay: true,
       path: "./json/social-phone.json"
     });
+  window.addEventListener('resize', function () {
+    anim.resize();
+  });
 }
+let stopFirstFeatures = true;
+let stopSecondFeatures = true;
 
 function showOnScroll() {
   $('.js-screen').each(function () {
+
+
     let startPosition = 0;
     let windowStartPosition = $(window).scrollTop();
     let elem = $(this);
     let sectionPos = elem.offset().top;
     let windowPos = windowStartPosition + $(window).height() / 1.8;
-    let musicPosition = windowStartPosition + $(window).height();
+    // let musicPosition = windowStartPosition + $(window).height();
 
     if (windowStartPosition > startPosition) {
       $('.header').addClass('scrolled');
       logo.addClass('logo_animated');
-
+      logo.removeClass('animate');
     } else {
       $('.header').removeClass('scrolled');
-      logo.removeClass('logo_animated');
     }
     if (sectionPos < windowPos) {
       elem.addClass('active');
@@ -316,10 +380,96 @@ function showOnScroll() {
         socialFlag = false;
       }
     }
+    // if ($('[data-slide="1"]').hasClass('show')) {
+    //   // console.log(stopFlag);
+    //   console.log('first slide')
+    //   if (stopFirstFeatures) {
+    //     // disableScroll();
+    //     body.addClass('no-scroll');
+
+    //     setTimeout(function () {
+    //       // enableScroll()
+    //       body.removeClass('no-scroll');
+
+    //     }, 1000);
+    //     stopFirstFeatures = false;
+    //   } else {
+    //     stopFirstFeatures = true;
+
+    //   }
+    // }
+    // // stopFlag = true;
+    // if ($('[data-slide="2"]').hasClass('show')) {
+    //   if (stopSecondFeatures) {
+    //     // disableScroll();
+    //     body.addClass('no-scroll');
+    //     setTimeout(function () {
+    //       // enableScroll()
+    //       body.removeClass('no-scroll');
+    //     }, 1000);
+    //     stopSecondFeatures = false;
+    //   }
+    // }
   });
 }
 
+// function pauseOnSection() {
+// if (stopFlag) {
+//   disableScroll();
+//   setTimeout(function () {
+//     stopFlag = false;
+//     enableScroll()
+//   }, 1000);
+// }
+// }
+
 $(document).ready(function () {
+
+  // const swiper = new Swiper('.section__slider__sticky', {
+  //   direction: 'horizontal',
+  //   slidePerView: 1,
+  //   // slidesPerGroup: 1,
+  //   keyboard: {
+  //     enable: true,
+  //     // onlyInViewport: true,
+  //     pageUpDown: true,
+  //   },
+  //   mousewheel: {
+  //     sensitivity: 1,
+  //   },
+  //   // watchOverflow: true,
+  //   // speed: 800,
+  //   // observer: true,
+  //   // obervePrents: true,
+  //   // observeSlideChildren: true,
+  //   // effect: 'fade',
+  //   simulateTouch: true,
+  //   on: {
+  //     slideChange: function () {
+  //       console.log(swiper.activeIndex);
+  //       console.log(swiper.clickedIndex);
+
+  //     },
+  //     reachEnd: function () {
+  //       console.log('last');
+  //       simulateTouch: false;
+  //     },
+  //     reachBeginning: function () {
+  //       // console.log('last');
+  //       simulateTouch: true;
+  //     },
+  //   }
+  // });
+
+  // swiper.allowTouchMove = true;
+  $('[data-slide="1"], [data-slide="2"], [data-slide="3"], [data-slide="4"], [data-slide="5"], [data-slide="6"]').mouseover(function () {
+    console.log('hover1');
+    disableScroll();
+    setTimeout(function () {
+      enableScroll();
+      // $('[data-slide="1"]').css('pointer-events', 'none');
+    }, 100);
+  });
   // jQuery.scrollSpeed(100, 100);
 
   console.log('DOM завантажено');
@@ -409,8 +559,6 @@ $(document).ready(function () {
     const sliderSectionTop = sliderSection.offset().top;
     const sliderSectionBottom = (sliderSectionTop + sliderSection.height()); // minus offset
     const sliderSectionHeight = sliderSection.height();
-    // console.log(sectionHeight);
-
 
     $(window).scroll(function () {
       $('.section__slider__sticky').stick_in_parent();
@@ -421,58 +569,174 @@ $(document).ready(function () {
         let ss = scrollPosition - sliderSectionTop; // window scroll - section offset() top;
         let percent = (sliderSectionHeight - sliderSectionTop) / 100;
         let percentage = ss / percent;
-        console.log(percentage);
+        // console.log(percentage);
 
-
-        if (percentage < 16, 666 && percentage > 0) {
+        if (percentage <= 16, 666 && percentage > 0) {
           $('.slide__item').removeClass('show');
           // $('.slide__item').addClass('showed');
           $('[data-slide="1"]').addClass('show');
-          // $('[data-slide="1"]').removeClass('showed');
-          // $('.slide__item').removeClass('showed');
-
+          $('[data-slide="1"]').hover(function () {
+            disableScroll();
+            setTimeout(function () {
+              enableScroll();
+            }, 100);
+          });
         }
-        if (percentage < 33.333 && percentage > 16.666) {
+
+        if (percentage <= 33.333 && percentage > 16.666) {
           $('.slide__item').removeClass('show');
           $('[data-slide="2"]').addClass('show');
+          // stopFlag = true;
+          $('[data-slide="2"]').hover(function () {
+            disableScroll();
+            setTimeout(function () {
+              enableScroll();
+            }, 100);
+          });
+          // pauseOnSection()
+          // if (stopFlag) {
+          //   disableScroll();
+          //   setTimeout(function () {
+          //     enableScroll()
+          //     // stopFlag = false;
+          //   }, 1000);
+          // }
+          // console.log(stopFlag);
+
           // $('[data-slide="1"]').addClass('showed');
         }
-        if (percentage < 49.93 && percentage > 33.333) {
+        if (percentage <= 49.93 && percentage > 33.333) {
           $('.slide__item').removeClass('show');
           $('[data-slide="3"]').addClass('show');
+          $('[data-slide="3"]').hover(function () {
+            disableScroll();
+            setTimeout(function () {
+              enableScroll();
+            }, 100);
+          });
+          // pauseOnSection()
+          // stopFlag = true;
+
+          // if (stopFlag) {
+          //   disableScroll();
+          //   setTimeout(function () {
+          //     // stopFlag = false;
+          //     enableScroll()
+          //   }, 1000);
+          // }
+          // stopFlag = false;
+
         }
-        if (percentage < 66.593 && percentage > 49.93) {
+        if (percentage <= 66.593 && percentage > 49.93) {
           $('.slide__item').removeClass('show');
           $('[data-slide="4"]').addClass('show');
+          $('[data-slide="4"]').hover(function () {
+            disableScroll();
+            setTimeout(function () {
+              enableScroll();
+            }, 100);
+          });
+          // pauseOnSection()
+          // stopFlag = true;
+
+          // if (stopFlag) {
+          //   disableScroll();
+          //   setTimeout(function () {
+          //     enableScroll()
+          //   }, 1000);
+          //   // stopFlag = false;
+
+          // }
+          // stopFlag = false;
+
         }
-        if (percentage < 83.259 && percentage > 66.593) {
+        if (percentage <= 83.259 && percentage > 66.593) {
           $('.slide__item').removeClass('show');
           $('[data-slide="5"]').addClass('show');
+          $('[data-slide="5"]').hover(function () {
+            disableScroll();
+            setTimeout(function () {
+              enableScroll();
+            }, 100);
+          });
+          // pauseOnSection()
+          // stopFlag = true;
+
+          // if (stopFlag) {
+          //   disableScroll();
+          //   setTimeout(function () {
+          //     stopFlag = false;
+          //     enableScroll()
+          //   }, 1000);
+          // }
+          // stopFlag = false;
+
         }
-        if (percentage > 83.259 && percentage < 100) {
+        if (percentage > 83.259 && percentage <= 100) {
           $('.slide__item').removeClass('show');
           $('[data-slide="6"]').addClass('show');
+          // pauseOnSection()
+          // stopFlag = true;
+          $('[data-slide="6"]').hover(function () {
+            disableScroll();
+            setTimeout(function () {
+              enableScroll();
+            }, 100);
+          });
+
+          // if (stopFlag) {
+          //   disableScroll();
+          //   setTimeout(function () {
+          //     stopFlag = false;
+          //     enableScroll()
+          //   }, 1000);
+          // }
+          // stopFlag = false;
+
         }
         if (percentage >= 100) {
           $('.slide__item').removeClass('show');
           percentage = 100;
         }
-        if (percentage <= 2) {
-          $('.slide__item').removeClass('show');
-          percentage = 1;
-        }
+        // if (percentage <= 2) {
+        //   $('.slide__item').removeClass('show');
+        //   percentage = 1;
+        // }
         // if (percentage >= 100) {
         // }
         // if (percentage <= 2) {
         // }
-        console.log(percentage);
+        // console.log(percentage);
       }
     });
   }
+
+  let startScroll = 0;
   $(window).scroll(function () {
     const scrollValue = $(this).scrollTop();
     showOnScroll(scrollValue);
     scrollValue >= 1 ? closeMenu() : null;
+
+    let currentScrollPosition = $(this).scrollTop();
+
+    if (currentScrollPosition > startScroll) {
+      console.log('scroll down');
+
+    } else {
+      console.log('scroll up');
+      if (!$('.section__slider__sticky').hasClass('is_stuck')) {
+        console.log('dont');
+        $('.slide__item').removeClass('show');
+      }
+      console.log($('.download').scrollTop);
+      if (scrollValue <= $('[data-slide="1"]').offset().top && $('[data-slide="1"]').hasClass('show')) {
+        $('[data-slide="1"]').css('pointer-events', 'none');
+      } else {
+
+      }
+    }
+    startScroll = currentScrollPosition;
+
   })
 
 });
@@ -541,3 +805,36 @@ testWebP(function (support) {
     document.querySelector("body").classList.add("no-webp");
   }
 });
+
+
+
+// function loadData() {
+//   return new Promise((resolve, reject) => {
+//     // setTimeout не является частью решения
+//     // Код ниже должен быть заменен на логику подходящую для решения вашей задачи
+//     setTimeout(resolve, 2000);
+//   })
+// }
+
+// loadData()
+//   .then(() => {
+//     let preloaderEl = document.getElementById('preloader');
+//     preloaderEl.classList.add('hidden');
+//     preloaderEl.classList.remove('visible');
+//   });
+
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
